7b9a35f7622164868570ee9484aef9b3a9b5d4c9
BBharat01 <bbharat023@gmail.com>
2025-02-25 20:39:18 +0000

    Update Logging

--- src/main/java/com/teya/assignment/ledger/controller/LedgerController.java
+++ src/main/java/com/teya/assignment/ledger/controller/LedgerController.java
@@ -4,8 +4,14 @@ import com.teya.assignment.ledger.model.Transaction;
 import com.teya.assignment.ledger.service.LedgerService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -14,18 +20,21 @@ import java.util.List;
 @Tag(name = "Ledger API", description = "APIs for managing ledger transactions")
 public class LedgerController {
 
+    private static final Logger log = LoggerFactory.getLogger(LedgerController.class);
     @Autowired
     private LedgerService ledgerService;
 
     @PostMapping("/deposit")
     @Operation(summary = "Deposit Money", description = "Deposits a specified amount into the ledger")
     public Transaction deposit(@RequestParam double amount) {
+        log.info("Depositing {} into account",amount);
         return ledgerService.recordTransaction("DEPOSIT", amount);
     }
 
     @PostMapping("/withdraw")
     @Operation(summary = "Withdraw Money", description = "Withdraws a specified amount from the ledger")
     public Transaction withdraw(@RequestParam double amount) {
+        log.info("Withdrawing {} from account",amount);
         return ledgerService.recordTransaction("WITHDRAWAL", amount);
     }
 
--- src/main/java/com/teya/assignment/ledger/service/LedgerService.java
+++ src/main/java/com/teya/assignment/ledger/service/LedgerService.java
@@ -2,6 +2,8 @@ package com.teya.assignment.ledger.service;
 
 import com.teya.assignment.ledger.model.Transaction;
 import com.teya.assignment.ledger.repository.TransactionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -9,12 +11,18 @@ import java.util.List;
 @Service
 public class LedgerService {
 
+    private static final Logger log = LoggerFactory.getLogger(LedgerService.class);
     @Autowired
     private TransactionRepository transactionRepository;
 
     public Transaction recordTransaction(String type, double amount) {
-        if (amount <= 0) throw new IllegalArgumentException("Amount must be positive");
+        if (amount <= 0) {
+            log.error("Value of Amount should be positive , current value is {}",amount);
+            throw new IllegalArgumentException("Amount must be positive");
+        }
         if (type.equalsIgnoreCase("WITHDRAWAL") && getBalance() < amount) {
+            log.error("Balance is less than the amount requested, " +
+                    "current balance is {} and amount requested is {}",getBalance(),amount);
             throw new IllegalArgumentException("Insufficient balance");
         }
         Transaction transaction = new Transaction(type, amount);
